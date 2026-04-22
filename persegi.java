d071cd70843fbd466f8c9d0eee34c0e9095ef591
Nasrul <97945936+nasrul12-sudo@users.noreply.github.com>
2026-04-22 15:24:28 +0700

    Delete persegi.java

--- persegi.java
+++
@@ -1,9 +0,0 @@
-public class persegi {
-    public double sisi;
-    public double hitungKeliling(){
-        return 4 *sisi;
-    }
-    public double hitungLuas(){
-        return sisi * sisi;
-    }
-}
